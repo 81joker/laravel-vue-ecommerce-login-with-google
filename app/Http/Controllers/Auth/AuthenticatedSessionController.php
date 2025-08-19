fd0c5db237a2d2516339bb3f99682e5032470033
Nehad <35563834+81joker@users.noreply.github.com>
2025-08-19 12:17:59 +0200

    ABC-123: Implement Google login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -67,8 +67,6 @@ class AuthenticatedSessionController extends Controller
             ['email' => $user->email],
             [
                 'name' => $user->name,
-                // 'email' => $user->getEmail(),
-                // 'avatar' => $user->getAvatar(),
                 'password' => Hash::make(Str::random(16)), // Set a default password or handle it as needed
                 ]
         );
